d3da02be3fecb0d36deb31a41e942227ad51665d
mayank gupta <mayankguptamg462@gmail.com>
2022-11-11 00:17:10 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import '../styles/App.css';
 const arr = JSON.parse(window.localStorage.getItem('props') || `["hello","world"]`) // do not change
+
 const Join = (props) =>{
+  
   return(
     <div id ="join">
+      {props.words.join(',')}
       {/* Access prop 'words' and print it using .join like words.join(',')*/}
     </div>
   )
@@ -12,7 +15,7 @@ const App = () => {
 
   return (
     <div id="main">
-      <Join  />
+      <Join words={arr}  />
     </div>
   )
 }
